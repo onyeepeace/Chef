c4d0f8d158508939b92fe1095562e8b55549b8c5
Peace <onyeepeaz@gmail.com>
2020-10-01 23:50:09 +0100

    menu navigation

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 // Select element function
 const selectElement = function (element) {
-    return document.querySelector(element);
+  return document.querySelector(element);
 };
-  
-let menuToggler = selectElement('.menu-toggle');
-let body = selectElement('body');
 
-menuToggler.addEventListener('click', function () {
-body.classList.toggle('open');
+let menuToggler = selectElement(".menu-toggle");
+let body = selectElement("body");
+
+menuToggler.addEventListener("click", function () {
+  body.classList.toggle("open");
 });
+
+// Menu image hover
+let ul = document.querySelector(".nav-list");
+let home = document.querySelector(".home");
+let about = document.querySelector(".about");
+let cakes = document.querySelector(".cakes");
+let contact = document.querySelector(".contact");
+
+// home.addEventListener("mouseover", function (e) {
+//   e.preventDefault();
+//   // ul.style.backgroundImage = "url('images/ed2e735d-cf18-4455-8c0e-ef9c64058b2f.png')";
+//   ul.style.backgroundColor = "red";
+// });
+
+// home.addEventListener("mouseout", function (e) {
+//   e.preventDefault();
+//   // ul.style.backgroundImage = "url('images/ed2e735d-cf18-4455-8c0e-ef9c64058b2f.png')";
+//   ul.style.backgroundColor = "blue";
+// });
