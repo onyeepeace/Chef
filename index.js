2245e5009c0efec9e3212f5bfabaa79f6cfabe20
Peace <onyeepeaz@gmail.com>
2020-10-02 23:42:12 +0100

    menu animation

--- index.js
+++ index.js
@@ -17,14 +17,53 @@ let about = document.querySelector(".about");
 let cakes = document.querySelector(".cakes");
 let contact = document.querySelector(".contact");
 
-// home.addEventListener("mouseover", function (e) {
-//   e.preventDefault();
-//   // ul.style.backgroundImage = "url('images/ed2e735d-cf18-4455-8c0e-ef9c64058b2f.png')";
-//   ul.style.backgroundColor = "red";
-// });
+home.addEventListener("mouseover", function (e) {
+  e.preventDefault();
+  ul.style.backgroundImage =
+    "url('images/ed2e735d-cf18-4455-8c0e-ef9c64058b2f.png')";
+});
 
-// home.addEventListener("mouseout", function (e) {
-//   e.preventDefault();
-//   // ul.style.backgroundImage = "url('images/ed2e735d-cf18-4455-8c0e-ef9c64058b2f.png')";
-//   ul.style.backgroundColor = "blue";
-// });
+home.addEventListener("mouseout", function (e) {
+  e.preventDefault();
+  ul.style.backgroundColor = "#777";
+  ul.style.backgroundImage = "none";
+});
+
+about.addEventListener("mouseover", function (e) {
+  e.preventDefault();
+  ul.style.backgroundImage =
+    "url('images/2740810b97c20a4ec01265845c9dcbb5.jpg')";
+  ul.style.backgroundRepeat = "no-repeat";
+  ul.style.backgroundSize = "contain";
+  ul.style.backgroundPosition = "center";
+});
+
+about.addEventListener("mouseout", function (e) {
+  e.preventDefault();
+  ul.style.backgroundColor = "#777";
+  ul.style.backgroundImage = "none";
+});
+
+cakes.addEventListener("mouseover", function (e) {
+  e.preventDefault();
+  ul.style.backgroundImage =
+    "url('images/ed2e735d-cf18-4455-8c0e-ef9c64058b2f.png')";
+});
+
+cakes.addEventListener("mouseout", function (e) {
+  e.preventDefault();
+  ul.style.backgroundColor = "#777";
+  ul.style.backgroundImage = "none";
+});
+
+contact.addEventListener("mouseover", function (e) {
+  e.preventDefault();
+  ul.style.backgroundImage =
+    "url('images/6c910cab1c2afff4887c66d979856f5d.jpg')";
+});
+
+contact.addEventListener("mouseout", function (e) {
+  e.preventDefault();
+  ul.style.backgroundColor = "#777";
+  ul.style.backgroundImage = "none";
+});
