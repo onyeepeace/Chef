3985f921688dba5327c30867ccd865cf4320d2d7
Peace <onyeepeaz@gmail.com>
2020-12-26 17:47:38 +0100

    new styles added

--- index.js
+++ index.js
@@ -7,34 +7,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
-
-// scroll reveal
-window.sr = ScrollReveal();
-
-sr.reveal('.animate-left', {
-  origin: 'left',
-  duration: 1000,
-  distance: '25rem',
-  delay: 300
-});
-
-sr.reveal('.animate-right', {
-  origin: 'right',
-  duration: 1000,
-  distance: '25rem',
-  delay: 300
-});
-
-sr.reveal('.animate-top', {
-  origin: 'top',
-  duration: 1000,
-  distance: '25rem',
-  delay: 600
-});
-
-sr.reveal('.animate-bottom', {
-  origin: 'bottom',
-  duration: 1000,
-  distance: '25rem',
-  delay: 600
-});
